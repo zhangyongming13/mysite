ba6a56fb6290d270ade7d0ce8f0aa952b55397f8
zhang <mingtiantianshi13@163.com>
2019-06-02 22:38:21 +0800

    201906022236验证码比较的时候，忽略大小写，这样更人性化；使用网易邮箱发送验证码；返回邮箱发送错误更加清晰；优化了一些细节

--- mysite/settings.py
+++ mysite/settings.py
@@ -208,9 +208,9 @@ CKEDITOR_CONFIGS = {
 # https://docs.djangoproject.com/en/2.0/ref/settings/#email
 # https://docs.djangoproject.com/en/2.0/topics/email/
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.qq.com'
+EMAIL_HOST = 'smtp.163.com'
 EMAIL_PORT = 25
-EMAIL_HOST_USER = '790454963@qq.com'
-EMAIL_HOST_PASSWORD = 'wbtroqgareybbfie'  # 授权码
+EMAIL_HOST_USER = 'mingtiantianshi16@163.com'
+EMAIL_HOST_PASSWORD = '19940327ming'  # 授权码
 EMAIL_SUBJECT_PREFIX = '[小明的博客] '
 EMAIL_USE_TLS = True  # 与SMTP服务器通信时，是否启动TLS链接(安全链接)
