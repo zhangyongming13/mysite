c5ff456e8b760a037b5569fcd8e14f5f05a2ad9a
zhang <mingtiantianshi13@163.com>
2019-06-02 11:40:31 +0800

    201906021140数据库换成mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,10 +84,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# 使用mysql的数据库
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite_db',
+        'USER': 'zhang',
+        'PASSWORD': '19940327',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
