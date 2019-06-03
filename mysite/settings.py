58d25fd4c1e9296bb31286c38a7a04d096ad3491
zhang <mingtiantianshi13@163.com>
2019-06-03 23:45:44 +0800

    201906032345设置收集CSS文件的目录

--- mysite/settings.py
+++ mysite/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')  # 设置将其他目录的css文件收集到一个目录中
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
